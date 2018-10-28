b1198a39f6012cfa07a15a0b2e282168b56d8aff
meisbokai <e0176277@u.nus.edu>
2018-10-28 19:36:10 +0800

    added in Adapted Attendance. Currently not working yet

---
+++ src/seedu/addressbook/storage/jaxb/AdaptedAttendance.java
@@ -0,0 +1,67 @@
+package seedu.addressbook.storage.jaxb;
+
+import javax.xml.bind.annotation.XmlValue;
+
+import seedu.addressbook.common.Utils;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Attendance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * JAXB-friendly adapted attendance data holder class.
+ */
+public class AdaptedAttendance {
+
+    @XmlValue
+    private List<String> attendanceDate;
+
+    @XmlValue
+    private List<String> attendanceIsPresent;
+
+    /**
+     * No-arg constructor for JAXB use.
+     */
+    public AdaptedAttendance() {}
+
+    /**
+     * Converts a given attendance into this class for JAXB use.
+     *
+     * @param source future changes to this will not affect the created AdaptedTag
+     */
+    public AdaptedAttendance(Attendance source) {
+        for(Map.Entry entry: source.attendancePersonMap.entrySet()) {
+            if (entry.getValue().equals(true)){
+                attendanceIsPresent.add("Present");
+            } else {
+                attendanceIsPresent.add("Absent");
+            }
+        }
+
+        attendanceDate = new ArrayList<>(source.attendancePersonMap.keySet());
+    }
+
+    /**
+     * Returns true if any required field is missing.
+     *
+     * JAXB does not enforce (required = true) without a given XML schema.
+     * Since we do most of our validation using the data class constructors, the only extra logic we need
+     * is to ensure that every xml element in the document is present. JAXB sets missing elements as null,
+     * so we check for that.
+     */
+    public boolean isAnyRequiredFieldMissing() {
+        return Utils.isAnyNull(attendanceDate, attendanceIsPresent);
+    }
+
+    /**
+     * Converts this jaxb-friendly adapted attendance object into the Attendance object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person
+     */
+//    public Attendance toModelType() throws IllegalValueException {
+//        return new Attendance(attendanceDate, attendanceIsPresent);
+//    }
+}
