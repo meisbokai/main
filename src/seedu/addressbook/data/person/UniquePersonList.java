f6a30f85540869acde6c8324f777a1972aaf47cf
meisbokai <e0176277@u.nus.edu>
2018-11-01 10:00:30 +0800

    added method to return list of absent people

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -170,7 +170,7 @@ public class UniquePersonList implements Iterable<Person> {
 
     /** Iterates through the UniquePersonList to check the attendance of each person
      * @param date of which list of present people should generate
-     * @return A list of present people
+     * @return A list of present/absent/no attendance people
      */
     public List listOfPresentPeople (String date) {
         List <Name> listOfPresent = new ArrayList<>();
@@ -181,6 +181,17 @@ public class UniquePersonList implements Iterable<Person> {
             }
         }
         return listOfPresent;
+    }
+
+    public List listOfAbsentPeople (String date) {
+        List <Name> listOfAbsent = new ArrayList<>();
+        for (Person p: internalList) {
+            Boolean isPresent = p.viewAttendanceDateMethod(date);
+            if (!isPresent) {
+                listOfAbsent.add(p.getName());
+            }
+        }
+        return listOfAbsent;
     }
 
     /**
