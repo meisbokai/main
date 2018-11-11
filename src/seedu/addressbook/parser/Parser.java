ada3f20d9455eb1bb9e1f782ee534fd5a7e8d553
meisbokai <e0176277@u.nus.edu>
2018-11-11 17:30:02 +0800

    change error message for attendance commands that require INDEX

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -579,7 +579,8 @@ public class Parser {
                     matcher.group("date"),
                     isPresentBool);
         } catch (NumberFormatException nfe) { //do the most specific catch on top
-            return new IncorrectCommand(nfe.getMessage());
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    UpdateAttendanceCommand.MESSAGE_USAGE));
         } catch (java.text.ParseException pe) {
             return new IncorrectCommand(MESSAGE_DATE_CONSTRAINTS);
         } catch (IllegalValueException ive) {
@@ -613,12 +614,12 @@ public class Parser {
                     matcher.group("date"),
                     isPresentBool);
         } catch (NumberFormatException nfe) { //do the most specific catch on top
-            return new IncorrectCommand(nfe.getMessage());
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    ReplaceAttendanceCommand.MESSAGE_USAGE));
         } catch (java.text.ParseException pe) {
             return new IncorrectCommand(MESSAGE_DATE_CONSTRAINTS);
         } catch (IllegalValueException ive) {
-            return new IncorrectCommand(MESSAGE_DATE_CONSTRAINTS + MESSAGE_DATE_CONSTRAINTS
-                    + ReplaceAttendanceCommand.MESSAGE_USAGE);
+            return new IncorrectCommand(MESSAGE_DATE_CONSTRAINTS + ReplaceAttendanceCommand.MESSAGE_USAGE);
         }
 
     }
@@ -638,7 +639,8 @@ public class Parser {
             final int targetIndex = parseInt(matcher.group("targetIndex"));
             return new ViewAttendancePersonCommand(targetIndex);
         } catch (NumberFormatException nfe) { //do the most specific catch on top
-            return new IncorrectCommand(nfe.getMessage());
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    ViewAttendancePersonCommand.MESSAGE_USAGE));
         }
     }
 
